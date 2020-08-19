1dc7c98417858d9a7a8aef30d04f3adecbdc83c0
sugino <git@sugi.cc>
2020-08-19 21:52:51 +0900

    add lightCookie texture

---
+++ Assets/Rotater.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotater : MonoBehaviour
+{
+    public float angle = 45f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.up * angle * Time.deltaTime, Space.Self);
+    }
+}
